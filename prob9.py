b90e82f8b1919d67b06139663c59114f762943d3
GitDavid <David.Liang@nyu.edu>
2017-04-06 21:41:36 -0400

    prob9

---
+++ prob9.py
@@ -0,0 +1,25 @@
+def is_pytrip(a, b, c):
+	'''checks for pythagorean triplet'''
+	if (a ** 2) + (b ** 2) == (c **2):
+		return True
+	else:
+		return False
+
+
+'''iterates through all combinations of i and j to find
+py triplet that sums to 1000'''
+
+i = 1
+
+while i < 1000:
+	j = 1
+	while j < 1000:
+		k = 1000 - i - j
+		# print "running: " + str(i) + ", " + str(j) + ", " + str(k)
+		if (is_pytrip(i, j, k) and (i + j + k == 1000)):
+			print "Solution is: " + str(i) + ", " + str(j) + ", " + str(k)
+			break
+		j += 1
+	i += 1
+
+print "done"
