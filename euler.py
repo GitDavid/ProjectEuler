18e863be0d20e3cd2fd1e95e8f742ea6a4c73893
David Liang <David.Liang@nyu.edu>
2017-06-14 07:36:47 -0400

    added prime functions

--- euler.py
+++ euler.py
@@ -1,3 +1,5 @@
+import math
+
 '''
 library created for commonly used Project Euler problems
 '''
@@ -7,19 +9,19 @@ def is_prime(x):
     returns True if x is prime; False if not
     basic checks for lower numbers and all primes = 6k +/-1
     '''
-    if n == 1:
+    if x == 1 or x == 0:
         return False
-    elif n <= 3:
+    elif x <= 3:
         return True
-    elif n % 2 == 0 or n % 3 == 0:
+    elif x % 2 == 0 or x % 3 == 0:
         return False
-    elif n < 9:
+    elif x < 9:
         return True
     else:
-        r = math.floor(math.sqrt(n))
+        r = math.floor(math.sqrt(x))
         f = 5
         while f <= r:
-            if (n % f == 0) or (n % (f + 2) == 0):
+            if (x % f == 0) or (x % (f + 2) == 0):
                 return False
             f += 6
         return True
