d593ea015785241f361819b95d2bbc49fa657867
David Liang <David.Liang@nyu.edu>
2017-06-14 07:16:41 -0400

    revised recur_len

--- prob26.py
+++ prob26.py
@@ -1,17 +1,20 @@
 from decimal import *
+from euler import *
 
 n = 1000
-getcontext().prec = 2 * n
+getcontext().prec = 2 * n #set decimal precision to this b/c n-1 is longest possible repeat
 
-#fix recur_len so that it checks for the case where repeating starts after the first char
+#need to fix recur_len so that it checks for the case where repeating starts after the first char
+#need to make recur_len not lazy, so it searches beyond first match
 
 def recur_len(str):
-
+    if len(str) <= 10: #to rule out terminating decimals
+        return 0
     first = str[0]
     first_next_idx = str[1:].index(first) + 1
     pattern1 = str[:first_next_idx]
     pattern2 = str[first_next_idx: first_next_idx + len(pattern1)]
-    print first, first_next_idx, pattern1, pattern2
+    # print first, first_next_idx, pattern1, pattern2
     while True:
         try:
             if pattern1 == pattern2:
@@ -19,18 +22,22 @@ def recur_len(str):
             else:
                 first_next_idx += str[first_next_idx + 1:].index(first) + 1
                 pattern2 = str[first_next_idx: first_next_idx + len(pattern1)]
-                print first, first_next_idx, pattern1, pattern2
+                # print first, first_next_idx, pattern1, pattern2
         except ValueError:
             return 0
 
-longest = 0
-for i in range(2, 1001):
-    print i
-    reciprocal = str(Decimal(1) / Decimal(i))
-    if len(reciprocal) <= 5 || recur_len(reciprocal[2:0] == 0):
-        pass
-    else:
-        if recur_len(reciprocal[2:]) > longest:
-            longest = i
+longest_dec = 0
+longest_i = 0
 
-print longest
+primes = [x for x in range(n) if is_prime(x)]
+
+# print primes
+
+for i in primes:
+    # print i
+    reciprocal = str(Decimal(1) / Decimal(i))
+    print i, recur_len(reciprocal[2:])
+    if recur_len(reciprocal[2:]) > longest_dec:
+        longest_i = i
+
+print longest_i
